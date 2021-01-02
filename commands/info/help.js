157fb430a9f5b90c5cd6039b0942961cf5468457
Glitch (muzikboto) <none>
2021-01-02 04:58:43 +0000

    Rewound to commit 96e18794583822f997b09dbb799a96f1ca143c4f + 0 edits

--- commands/info/help.js
+++ commands/info/help.js
@@ -15,27 +15,16 @@ module.exports = {
         `
 
 **MUSIC COMMANDS**
-\`__**MODERATION**__ 🌸
-`.kick,warn,ban,unban,purge,lock,unlock,mute,unmute,changenick,nuke,giverole`
+\`play[p],search,pause,resume,stop,skip,skipall,skipto,nowplaying[np],queue,loop,remove,volume\`
 
-🌸__**INFO**__
-`serverinfo,userinfo,serverstats,calendar`
 
-🌸 __**ECONOMY**__ 🌸
-`bal,dep,withdraw,hunt,crime,beg,heist,rob,daily,weekly`
-```meme,search,emoji,kill,kiss,shoot,hack cry,hug,nitro,bully pat,cat,```
+**INFO COMMANDS**
+\`ping,help\`
 
-🌸 __**TICKET**__ 🌸
-`.ticket - (creat a ticket)
-.c ticket - (close the ticket)`
-
-🌸 __**MUSIC**__ 🌸
-`.play | .pause |.stop
-
-🌸 __**BOT INFO**__ 🌸
-`.botinfo,.bugreport,.freepremium,.inv,`
-[**INVITE ME**](https://discord.com/oauth2/authorize?client_id=781774443859935242&scope=bot&permissions=2147483647)]
-$footer[use . before all commands example .help
+__**ABOUT BOT**__
+A POWERFUL MUSIC BOT MADE 24/7 MUSIC PLAYERS 
+13+ COMMANDS
+`
       )
       .setFooter(message.guild);
     message.channel.send(embed);
